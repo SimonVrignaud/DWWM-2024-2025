a7df7745faf4238f82cdda403f70041d5cc7caca
Nolwenn <nolwenn.wm@afci-formation.fr>
2024-12-10 08:36:44 +0100

    cours bdd

--- 05-docker/01-dev/index.php
+++ 05-docker/01-dev/index.php
@@ -7,5 +7,8 @@
 </head>
 <body>
     <h1>Bravo, vous faite tourner un conteneur docker</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id assumenda distinctio architecto repellat quod quibusdam eligendi. Aperiam officia, non, consequatur possimus ut fugit culpa quod nobis tenetur harum, saepe sit.
+    </p>
 </body>
 </html>
