4e4717bec821a50e5fd936529de0b4b4fde4c194
Nolwenn <nolwenn.wm@afci-formation.fr>
2025-01-08 15:54:38 +0100

    exercice JS trouver les erreurs

--- 04-javascript/00-bonus/error/script.js
+++ 04-javascript/00-bonus/error/script.js
@@ -1,15 +1,16 @@
 /* 
     Trouver et corriger les 10 erreurs pour que cette page fonctionne parfaitement.
+    Tout les boutons doivent être fonctionnel et on peut cliquer sur les images du slider.
 */
 const slider = document.querySelector('slider');
 const bckg = document.querySelector(".bckg-close");
 const modal = document.querySelector(".modal");
 const left = document.querySelector(".left-part");
 const right = document.querySelector(".right-part");
-let readmore;
+const readmore = null;
 
 function activate(e) {
-    const items = document.querySelectorAll('.item');
+    const items = document.querySelector('.item');
     e.target.matches('next') && slider.append(items[0])
     e.target.matches('.prev') && slider.prepend(items[items.length-1]);
     if(e.target.matches(".item"))
@@ -24,11 +25,11 @@ function activate(e) {
         readmore.style.top = "-100vh";
         const h = readmore.parentElement.querySelector("h2");
         const p = readmore.parentElement.querySelector("p");
-        setTimeout(openModal, 400, h.textContent, p.textContent);
+        setTimeout(openModal(), 400, h.textContent, p.textContent);
     }
     if(e.target.matches(".bckg-close") && readmore)
     {
-        closeModal();
+        closemodal();
         setTimeout(()=>{
             readmore.style.top = "";
             readmore = undefined;
@@ -36,7 +37,7 @@ function activate(e) {
     }
 }
 
-document.addEventListener('click',activate,false);
+document.addEventListener('clique',activate,false);
 
 function openModal(h, p)
 {
@@ -50,7 +51,7 @@ function openModal(h, p)
     modal.style.top = "0";
     bckg.style.display = "block";
 }
-function closeModal()
+closeModal()
 {
     left.style.height = modal.scrollHeight +"px";
     right.style.height = modal.scrollHeight +"px";
--- 04-javascript/00-bonus/error/script_correction.js
+++ 04-javascript/00-bonus/error/script_correction.js
@@ -1,12 +1,16 @@
+// Le point pour la selection de classe.
 const slider = document.querySelector('.slider');
 const bckg = document.querySelector(".bckg-close");
 const modal = document.querySelector(".modal");
 const left = document.querySelector(".left-part");
 const right = document.querySelector(".right-part");
-let readmore;
+// La variable devant changer, un let, pas un const
+let readmore = null;
 
 function activate(e) {
+    // Il faut un querySelectorAll
     const items = document.querySelectorAll('.item');
+    // Le point pour la selection de classe.
     e.target.matches('.next') && slider.append(items[0])
     e.target.matches('.prev') && slider.prepend(items[items.length-1]);
     if(e.target.matches(".item"))
@@ -21,10 +25,12 @@ function activate(e) {
         readmore.style.top = "-100vh";
         const h = readmore.parentElement.querySelector("h2");
         const p = readmore.parentElement.querySelector("p");
+        // On n'appelle pas la fonction pour un callback
         setTimeout(openModal, 400, h.textContent, p.textContent);
     }
     if(e.target.matches(".bckg-close") && readmore)
     {
+        // La majuscule au nom de la fonction.
         closeModal();
         setTimeout(()=>{
             readmore.style.top = "";
@@ -33,20 +39,24 @@ function activate(e) {
     }
 }
 
+// Les nom d'évènement sont en anglais et en minuscule
 document.addEventListener('click',activate,false);
 
 function openModal(h, p)
 {
     left.style.height = modal.scrollHeight +"px";
+    // L'ajout de "px" aux valeurs chiffrés.
     right.style.height = modal.scrollHeight +"px";
     modal.querySelector("h2").textContent = h;
     modal.querySelector("p").textContent = p;
 
+    // le symbole "%" aux données chiffrés.
     left.style.left = "50%";
     right.style.right = "50%";
     modal.style.top = "0";
     bckg.style.display = "block";
 }
+// Il manque le mot clef "function"
 function closeModal()
 {
     left.style.height = modal.scrollHeight +"px";
